8c8a561153876435429bc7a956ff3bad61f04aca
Blair <blair.liikala@gmail.com>
2021-01-23 21:21:02 -0600

    Update addon.setup.php

--- ctrl_s/addon.setup.php
+++ ctrl_s/addon.setup.php
@@ -4,7 +4,7 @@ return array(
   'author'      => 'Your Majesty',
   'name'        => 'Ctrl_S',
   'description' => 'Command/Control+S to Save, cmd+d to Save New, cmd+g to Save Close',
-  'version'     => '2',
+  'version'     => '3',
   'namespace'   => 'ThingsThatShouldBeInCore\Ctrl_s_ext',
   'settings_exist' => false
 );
